0dc247928f15f7ea10c7974ab616d26455abef3a
Cesar Bolanos Portilla <cesar.bolanosportilla@gmail.com>
2018-07-11 16:15:13 -0500

    package management in init

---
+++ init.el
@@ -0,0 +1,49 @@
+;;;; ---- package management ----
+
+;; be sure not to load stale bytecode-compiled lisp
+(setq load-prefer-newer t)
+
+;; this is where all subtree packages are
+(defconst emacs-pkg-dir (concat user-emacs-directory "pkg"))
+
+;; load up f, and its dependencies s and dash, so we can use `f-glob'
+;; and `f-join'
+(dolist (pkg '("f.el" "dash.el" "s.el"))
+  (add-to-list 'load-path (concat emacs-pkg-dir "/" pkg)))
+(require 'f) (require 's) (require 'dash)
+
+;; helper function
+(defun expand-all-globs (root globs)
+  (let ((do-glob (lambda (glob) (f-glob (f-join root glob)))))
+    (apply 'nconc (mapcar do-glob globs))))
+
+;; now add all my pkg lisp directories
+(let* ((globs '("*" "*/lisp"))
+       (dirs (expand-all-globs emacs-pkg-dir globs)))
+  (dolist (dir dirs)
+    (when (file-directory-p dir)
+      (add-to-list 'load-path dir))))
+
+;; finally put my own site-lisp at the front of `load-path'
+(add-to-list 'load-path (concat user-emacs-directory "site-lisp"))
+
+;; we will use use-package to load everything else
+(require 'use-package)
+
+(defun pnh-reinit-libs ()
+  (interactive)
+  (let ((generated-autoload-file (concat user-emacs-directory "my-autoload.el")))
+    (dolist (d (directory-files (concat user-emacs-directory "lib") t "^[^\.]"))
+      (dolist (f (directory-files d t "\\.el$"))
+        (byte-compile-file f))
+      (update-directory-autoloads d))))
+
+(dolist (l (directory-files (concat user-emacs-directory "lib") nil "^[^\.]"))
+  (add-to-list 'load-path (concat user-emacs-directory "lib/" l))
+  (autoload (intern l) (concat l ".el")))
+
+(when (not (file-exists-p (concat user-emacs-directory "my-autoload.el")))
+  (pnh-reinit-libs))
+
+(load (concat user-emacs-directory "my-autoload.el"))
+
