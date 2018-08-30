ae7028192c9ccbf8562c787aa9f4739715b6f285
Cesar Bolanos Portilla <cesar.bolanosportilla@gmail.com>
2018-08-30 14:06:48 -0500

    custom options

--- init.el
+++ init.el
@@ -107,6 +107,10 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 ;; nix mode
 (require 'nix-mode)
 
+;; custom
+(setq inhibit-spash-screen t)
+(setq inhibit-startup-message t)
