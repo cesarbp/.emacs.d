e86119059f94f15bf99d87682429c4dd5c88ceb4
Cesar Bolanos Portilla <cesar.bolanosportilla@gmail.com>
2018-09-13 17:59:19 -0500

    testing autoload

--- init.el
+++ init.el
@@ -33,13 +33,13 @@
 (defun pnh-reinit-libs ()
   (interactive)
   (let ((generated-autoload-file (concat user-emacs-directory "my-autoload.el")))
-    (dolist (d (directory-files (concat user-emacs-directory "lib") t "^[^\.]"))
+    (dolist (d (directory-files (concat user-emacs-directory "pkg") t "^[^\.]"))
       (dolist (f (directory-files d t "\\.el$"))
         (byte-compile-file f))
       (update-directory-autoloads d))))
 
-(dolist (l (directory-files (concat user-emacs-directory "lib") nil "^[^\.]"))
-  (add-to-list 'load-path (concat user-emacs-directory "lib/" l))
+(dolist (l (directory-files (concat user-emacs-directory "pkg") nil "^[^\.]"))
+  (add-to-list 'load-path (concat user-emacs-directory "pkg/" l))
   (autoload (intern l) (concat l ".el")))
 
 (when (not (file-exists-p (concat user-emacs-directory "my-autoload.el")))
