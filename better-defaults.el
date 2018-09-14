60d1db61ff701c53d576980ba6a23ffa5058c678
Cesar Bolanos Portilla <cesar.bolanosportilla@gmail.com>
2018-09-14 18:00:26 -0500

    Squashed 'pkg/better-defaults/' changes from 90df5752a..ab830cf1a
    ab830cf1a Remove outdated install instructions.
    12d9e6dbf Merge pull request #23 from raxod502/feat/no-autoload
    63c550258 Don't use an autoload to set the better defaults
    6f502fd2d Remove unnecessary x-select-* settings.
    ce2a9a856 Merge pull request #20 from shackra/master
    ccf0ed08f small fix
    66710ad45 Solves bug #19
    3f993b021 Disable Horizontal Scroll Bar
    
    git-subtree-dir: pkg/better-defaults
    git-subtree-split: ab830cf1a0987f43e419565404a4fa8c0a2f5560

--- better-defaults.el
+++ better-defaults.el
@@ -41,16 +41,18 @@
 
 ;;; Code:
 
-;;;###autoload
 (progn
-  (ido-mode t)
-  (setq ido-enable-flex-matching t)
+  (unless (fboundp 'helm-mode)
+    (ido-mode t)
+    (setq ido-enable-flex-matching t))
 
   (menu-bar-mode -1)
   (when (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
   (when (fboundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
+  (when (fboundp 'horizontal-scroll-bar-mode)
+    (horizontal-scroll-bar-mode -1))
 
   (autoload 'zap-up-to-char "misc"
     "Kill up to, but not including ARGth occurrence of CHAR." t)
@@ -72,9 +74,7 @@
 
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
-  (setq x-select-enable-clipboard t
-        x-select-enable-primary t
-        save-interprogram-paste-before-kill t
+  (setq save-interprogram-paste-before-kill t
         apropos-do-all t
         mouse-yank-at-point t
         require-final-newline t
