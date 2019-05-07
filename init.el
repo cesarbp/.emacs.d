b625dd473703e7afca0baa5d5ea4d0e545e95255
César Bolaños Portilla <cesar.bolanosportilla@gmail.com>
2019-05-07 11:49:16 -0500

    remove this w3m stuff

--- init.el
+++ init.el
@@ -246,8 +246,6 @@
     ))
 (add-hook 'dante-mode-hook 'remove-dante-company-from-head)
 
-
-
 (condition-case nil
     (require 'haskell-mode-autoloads)
   (error (message "WARNING: haskell mode files need to be generated, see github README of haskell-mode. Need to do 'make haskell-mode-autoloads.el'")))
@@ -261,30 +259,6 @@
 (winner-mode 1)
 (put 'erase-buffer 'disabled nil)
 (setq shell-command-switch "-ic")
-;; w3m for haddock - requires w3m to be installed
-(defun setup-w3m ()
-  
-  (setq w3m-mode-map (make-sparse-keymap))
-
-  (define-key w3m-mode-map (kbd "RET") 'w3m-view-this-url)
-  (define-key w3m-mode-map (kbd "q") 'bury-buffer)
-  (define-key w3m-mode-map (kbd "<mouse-1>") 'w3m-maybe-url)
-  (define-key w3m-mode-map [f5] 'w3m-reload-this-page)
-  (define-key w3m-mode-map (kbd "C-c C-d") 'haskell-w3m-open-haddock)
-  (define-key w3m-mode-map (kbd "M-<left>") 'w3m-view-previous-page)
-  (define-key w3m-mode-map (kbd "M-<right>") 'w3m-view-next-page)
-  (define-key w3m-mode-map (kbd "M-.") 'w3m-haddock-find-tag))
-
-(defun w3m-maybe-url ()
-  (interactive)
-  (if (or (equal '(w3m-anchor) (get-text-property (point) 'face))
-          (equal '(w3m-arrived-anchor) (get-text-property (point) 'face)))
-      (w3m-view-this-url)))
-
-;; (condition-case err
-;;     (progn (require 'w3m)
-;;            (setup-w3m))
-;;   (error (message "WARNING: %s" (error-message-string err))))
 
 ;; sql 4 spaces
 (add-hook 'sql-mode-hook
