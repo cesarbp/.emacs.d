f1376ca94cdfaf46a9adbc141fa8701878247b85
César Bolaños Portilla <cesar.bolanosportilla@gmail.com>
2019-05-24 13:06:26 -0500

    Try more fonts

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 120 :foundry "nil" :family "Input Sans")))))
+ '(default ((t (:height 120 :family "SF Mono")))))
 
 ;;;; ---- package management ----
 
