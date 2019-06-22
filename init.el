41aef331f596cf06f2de2feb4b0f5d87a83becfb
Cesar Bolaños <cesar.bolanosportilla@gmail.com>
2019-06-22 13:00:28 -0500

    actually the color vector was fine

--- init.el
+++ init.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(ansi-color-names-vector
- ;;   ["#696969" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#515151"])
+ '(ansi-color-names-vector
+   ["#696969" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#515151"])
  '(custom-safe-themes
    (quote
     ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
