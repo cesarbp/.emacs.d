9bddcd4cd1441c595021027e8eb8e9bb8bdb8071
César Bolaños <cesar.bolanosportilla@gmail.com>
2020-04-04 20:11:16 -0600

    Move osx config to diff file and add super key

--- cbp/cosmetic.el
+++ cbp/cosmetic.el
@@ -15,17 +15,5 @@
 ;(global-linum-mode)
 (set-face-foreground 'linum "dim gray")
 
-;;; Mac OS config
-(when (string-equal system-type "darwin")
-    (setq mac-option-modifier nil)
-    (setq mac-command-modifier 'meta)
-    (setq ring-bell-function
-      (lambda ()
-        (let ((orig-bg (face-background 'mode-line)))
-          (set-face-background 'mode-line "#2F4F4F")
-          (run-with-idle-timer 0.1 nil
-                               (lambda (bg) (set-face-background 'mode-line bg))
-                               orig-bg)))))
-
 (when (display-graphic-p)
   (setq initial-frame-alist '((width . 180) (height . 55))))
