7a2f6361eeb57261ad3e1cf5fa31b393ef27a30b
César Bolaños <cesar.bolanosportilla@gmail.com>
2020-04-06 15:43:52 -0500

    Try ivy

--- init.el
+++ init.el
@@ -80,14 +80,36 @@
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
-;; ido
+;;; ido
 ;(require 'ido-completing-read+)
 ;(require 'flx-ido)
 ;;(ido-mode 1)        ; loaded by better-defaults
-(ido-everywhere 1)
-(ido-ubiquitous-mode 1)
-(flx-ido-mode 1)
-(ido-vertical-mode 1)
+;; (ido-everywhere 1)
+;; (ido-ubiquitous-mode 1)
+;; (flx-ido-mode 1)
+;; (ido-vertical-mode 1)
+;;; Ivy
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 ;; disable ido faces to see flx highlights.
 ;(setq ido-enable-flex-matching t)    ; loaded by better-defaults
 (setq ido-use-faces nil)
