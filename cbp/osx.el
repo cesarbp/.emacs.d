79db77ca579f039573018dd91d02eccafd561506
César Bolaños <cesar.bolanosportilla@gmail.com>
2020-06-22 13:22:44 -0500

    Switch left/right alt keys for mac

--- cbp/osx.el
+++ cbp/osx.el
@@ -1,12 +1,12 @@
 ;;; Mac OS config
 (when (string-equal system-type "darwin")
-    (setq mac-option-modifier nil)
-    (setq mac-command-modifier 'meta)
-    (setq mac-right-option-modifier 'super)
-    (setq ring-bell-function
-      (lambda ()
-        (let ((orig-bg (face-background 'mode-line)))
-          (set-face-background 'mode-line "#2F4F4F")
-          (run-with-idle-timer 0.1 nil
-                               (lambda (bg) (set-face-background 'mode-line bg))
-                               orig-bg)))))
+  (setq mac-option-modifier 'super)
+  (setq mac-command-modifier 'meta)
+  (setq mac-right-option-modifier 'nil)
+  (setq ring-bell-function
+        (lambda ()
+          (let ((orig-bg (face-background 'mode-line)))
+            (set-face-background 'mode-line "#2F4F4F")
+            (run-with-idle-timer 0.1 nil
+                                 (lambda (bg) (set-face-background 'mode-line bg))
+                                 orig-bg)))))
