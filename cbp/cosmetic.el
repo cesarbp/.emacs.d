77c077b1fc7efca2bf1230dc1c40eb753e765d95
César Bolaños <cesar.bolanosportilla@gmail.com>
2020-08-18 16:16:51 -0500

    Themes

--- cbp/cosmetic.el
+++ cbp/cosmetic.el
@@ -7,9 +7,10 @@
 ;; (add-to-list 'custom-theme-load-path (concat emacs-pkg-dir "/emacs-material-theme"))
 ;; (load-theme 'material)
 ;;; https://github.com/hlissner/emacs-doom-themes
-;; (add-to-list 'custom-theme-load-path (concat emacs-pkg-dir "/emacs-doom-themes/themes"))
-(add-to-list 'custom-theme-load-path (concat emacs-pkg-dir "/tron-legacy-emacs-theme"))
-(load-theme 'tron-legacy t)
+(add-to-list 'custom-theme-load-path (concat emacs-pkg-dir "/emacs-doom-themes/themes"))
+;; (add-to-list 'custom-theme-load-path (concat emacs-pkg-dir "/tron-legacy-emacs-theme"))
+(load-theme 'doom-vibrant t)
+;; (load-theme 'tron-legacy t)
 ;;; highlight current line number
 (require 'hlinum)
 (hlinum-activate)
